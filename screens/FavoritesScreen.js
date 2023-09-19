e3703b64321315f94600ed2f05766db2e5dd3732
Marc Mendoza <777marc@gmail.com>
2023-09-19 11:20:57 -0400

    implement favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { FavoritesContext } from "../store/context/favorites-context";
+import MealItem from "../components/MealItem";
 
-function FavoritesScreen() {
+function FavoritesScreen({ navigation }) {
+  const FavoriteMealCtx = useContext(FavoritesContext);
+
+  const displayMeals = MEALS.filter((meal) => {
+    return FavoriteMealCtx.ids.includes(meal.id);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Favorites</Text>
+      {displayMeals.map((dm, index) => {
+        const { id, title, imageUrl, affordability, complexity, duration } = dm;
+
+        const mealProps = {
+          id,
+          title,
+          imageUrl,
+          affordability,
+          complexity,
+          duration,
+        };
+
+        return <MealItem key={index} {...mealProps} />;
+      })}
     </View>
   );
 }
@@ -11,6 +34,7 @@ function FavoritesScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
   },
 });
 
